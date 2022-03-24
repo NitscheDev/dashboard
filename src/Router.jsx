6bf124e4d97991cf4edce060946eab05f720bc16
Rasmus Nitsche <nitschedev@gmail.com>
2022-03-24 08:51:43 +0100

    updated router

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,18 +5,18 @@ import Overview from './views/Overview'
 import NotFound from './components/NotFound'
 
 function Router() {
-  return (
-    <Routes>
-      <Route path='/dashboard' element={<Dashboard />} >
-        <Route path='' element={<Overview />} />
-        <Route path='/dashboard/posts'>
-          <Route path='/dashboard/posts/' element={<Posts />} />
-          <Route path='/dashboard/posts/add' element={<h1>Add Post</h1>} />
-        </Route>
-      </Route>
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route path='/dashboard' element={<Dashboard />} >
+                <Route path='' element={<Overview />} />
+                <Route path='/dashboard/posts'>
+                    <Route path='/dashboard/posts/' element={<Posts />} />
+                    <Route path='/dashboard/posts/add' element={<h1>Add Post</h1>} />
+                </Route>
+            </Route>
+            <Route path='*' element={<NotFound />} />
+        </Routes>
+    )
 }
 
 export default Router
